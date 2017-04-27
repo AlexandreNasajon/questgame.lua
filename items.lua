dd4cb2b6946e22d86811f0ae4b31d828c79e7e00
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-04-27 00:10:37 -0300

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,24 @@
+local Items = {}
+
+Items.bronze_sword = {
+  name = "Bronze Sword",
+  cost = 15,
+  power = 5
+} 
+Items.silver_sword = {
+  name = "Silver Sword",
+  cost = 30,
+  power = 10
+}
+
+Items.bronze_armor = {
+  name = "Bronze Armor",
+  cost = 20,
+  life = 15
+}
+Items.silver_armor = {
+  name = "Silver Armor",
+  cost = 40,
+  life = 30
+}
+return Items
