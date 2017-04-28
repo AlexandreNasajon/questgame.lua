96af15f480adb598a24468d5bd9fdea5cf0aaefb
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-04-28 00:55:43 -0300

    Create enemies.lua

---
+++ enemies.lua
@@ -0,0 +1,40 @@
+local Enemies = {}
+
+Enemies.bat = {
+  name = "Bat",
+  life = 5,
+  power = 1,
+  gold = 1
+}
+Enemies.spider = {
+    name = "Spider",
+    life = 8,
+    power = 2,
+    gold = 1,
+}
+Enemies.serpent = {
+    name = "Serpent",
+    life = 7,
+    power = 3,
+    gold = 1,
+}
+-- Enemies. = {
+--     name = "",
+--     life = ,
+--     power = ,
+--     gold = ,
+-- }
+-- Enemies. = {
+--     name = "",
+--     life = ,
+--     power = ,
+--     gold = ,
+-- }
+-- Enemies. = {
+--     name = "",
+--     life = ,
+--     power = ,
+--     gold = ,
+-- }
+
+return Enemies
