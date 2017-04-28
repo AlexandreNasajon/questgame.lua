d2ff150f0b9185751f9a935582c9ceabf703119a
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-04-28 00:55:04 -0300

    Update items.lua

--- items.lua
+++ items.lua
@@ -2,7 +2,7 @@ local Items = {}
 
 Items.bronze_sword = {
   name = "Bronze Sword",
-  cost = 15,
+  cost = 10,
   power = 5
 } 
 Items.silver_sword = {
@@ -10,7 +10,11 @@ Items.silver_sword = {
   cost = 30,
   power = 10
 }
-
+Items.golden_sword = {
+    name = "Golden Sword",
+    cost = 50,
+    power = 20
+}
 Items.bronze_armor = {
   name = "Bronze Armor",
   cost = 20,
@@ -20,5 +24,10 @@ Items.silver_armor = {
   name = "Silver Armor",
   cost = 40,
   life = 30
+}
+Items.golden_armor = {
+  name = "Golden Armor",
+  cost = 70,
+  life = 50
 }
 return Items
